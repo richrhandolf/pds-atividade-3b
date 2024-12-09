85d736184594a1ea3465e2b898e1cc270f032f35
Ricardo Rhandolf da Silva Souza <20211064010006@ifrn.local>
2024-12-09 18:06:55 -0300

    Projeto Pronto

---
+++ Pds/src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+		    public static void main(String[] args) {
+	        // Criando uma instância de Pessoa para cada estuantes
+	        Pessoa pessoa = new Pessoa("Ricardo", 19);
+
+	        // Exibindo as informações dos estudantes
+	        pessoa.exibirInformacoes();
+	    }
+
+}
